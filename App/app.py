414e42b28910e569760033620301683ed513465e
Edward Gaibor <78832141+gaiborjosue@users.noreply.github.com>
2023-04-25 19:59:41 -0400

    app_implementation-testing-model 💯

---
+++ App/app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pandas as pd
+import os
+
+#Import profile_report
+import ydata_profiling as pp
+from streamlit_pandas_profiling import st_profile_report
+
+# Machine Learning
+from pycaret.classification import setup, compare_models, pull, save_model
+
+with st.sidebar:
+    st.image("https://builtin.com/sites/www.builtin.com/files/styles/og/public/2021-12/machine-learning-examples-applications.png")
+    st.title("Auto Machine Learning Model Generator")
+    choice = st.radio("Navigation", ["Home", "Data Upload", "Profiling", "ML", "Download"])
+    st.info("This is an application to help you with DNS Traffic Control with Machine Learning")
+
+if os.path.exists("source.csv"):
+    df = pd.read_csv("source.csv", index_col=None)
+
+if choice == "Home":
+  st.title("Welcome to AutoML Model Generator")
+  st.write("Hello! This is a Machine Learning Model Generator. You can input your dataset and generate a Machine Learning Model. Also, you can download the model and use it for your own purpose. The model you are downloading is a pickle file, that contains the highest accuracy model selected by the AutoML. You can also access profiling report of your dataset and explore it.")
+  st.write("Under the Data Upload tab you can upload your dataset. The dataset should be in .csv format. After uploading the dataset, you can access the profiling report of your dataset under the Profiling tab. You can also generate a Machine Learning Model under the ML tab and see all the models compared side by side with their respective scores. After generating the model, you can download it under the Download tab.")
+  st.divider()
+
+if choice == "Data Upload":
+   st.title("Upload your dataset")
+   file = st.file_uploader("Upload your dataset here")
+   if file:
+      df = pd.read_csv(file, index_col=None)
+      df.to_csv("source.csv", index=None)
+      st.dataframe(df)
+      st.write("File uploaded successfully")
+
+
+if choice == "Profiling":
+    st.title("Exploratory Data Analysis")
+    if st.button("Generate Report"):
+        profile_report = df.profile_report()
+        st_profile_report(profile_report)
+
+
+if choice == "ML":
+    st.title("Machine Learning Generator")
+    target = st.selectbox("Select your target", df.columns)
+    if st.button("Train"):
+      setup(df, target=target)
+      setup_df = pull()
+      st.info("This is the ML Experiment")
+      st.dataframe(setup_df)
+      best_model = compare_models()
+      compare_models_df = pull()
+      st.info("This is the best model")
+      st.dataframe(compare_models_df)
+      best_model
+      save_model(best_model, "best_model")
+
+if choice == "Download":
+    with open("best_model.pkl", "rb") as f:
+        st.download_button("Download the model", f, "trained_model.pkl")
--- Readme.md
+++ Readme.md
@@ -1,11 +1,14 @@
 Model: [![Jupyter Notebook](https://img.shields.io/badge/jupyter-%23FA0F00.svg?style=for-the-badge&logo=jupyter&logoColor=white)](https://github.com/gaiborjosue/DNS-Traffic-Control/blob/master/Model/RandomForest.ipynb)
 
 # DNS Traffic Control Project
-Overview
+### Overview
 The DNS Traffic Control Project is a machine learning project that aims to classify DNS traffic as either benign or malicious using stateless datasets. The purpose of this project is to build a model that can accurately detect and prevent malicious DNS traffic.
 
 ## Dataset
 For this project, I used two stateless datasets containing features related to DNS traffic. The first dataset contains only benign traffic, while the second dataset contains both benign and malicious traffic. I used these datasets because they are readily available and provide a good starting point for building a DNS traffic classification model.
+
+### For the dataset source please refer to:
+  Samaneh Mahdavifar, Amgad Hanafy Salem, Princy Victor, Miguel Garzon, Amir H. Razavi, Natasha Hellberg, Arash Habibi Lashkari, “Lightweight Hybrid Detection of Data   Exfiltration using DNS based on Machine Learning”, The 11th IEEE International Conference on Communication and Network Security (ICCNS), Dec. 3-5, 2021, Beijing       Jiaotong University, Weihai, China.
 
 ## Class Definition
 To train the machine learning model, I defined the DNS anomalous detection class. This class has two possible values: 0 (benign) and 1 (malicious). I chose this class definition because it is a simple and intuitive way to classify DNS traffic.
@@ -13,7 +16,7 @@ To train the machine learning model, I defined the DNS anomalous detection class
 ## Methodology
 To build the DNS traffic classification model, I used a Random Forest Classifier algorithm from the scikit-learn library. The Random Forest Classifier is a popular algorithm for classification tasks, and it has been shown to be effective in detecting malicious traffic.
 
-I first merged the two datasets and shuffled the data to avoid bias. I then dropped the columns "longest_word" and "sld" from the dataset, as these columns were not relevant to the classification task. I also removed any rows with NaN values.
+I first merged the two datasets and shuffled the data to avoid bias. I also removed any rows with NaN values.
 
 I then normalized the numerical data using a MinMaxScaler object from the scikit-learn library. I split the data into training and testing sets using a 80-20 split, respectively.
 
